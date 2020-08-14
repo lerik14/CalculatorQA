8783b5a828a641cb8d78354c62a37b28f499a2fc
lerik14 <69652826+lerik14@users.noreply.github.com>
2020-08-14 14:22:54 -0400

    [CLC-1] Create a Calculator console application with default operations (#1)
    [CLC-1] Create a Calculator console application with default operations (sum, subtraction)
    
    Created a calculator class with two operations - sum and sub.

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calculator {
+
+    public void run() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Which operation to perform: ");
+        System.out.println("1. Sum");
+        System.out.println("2. Sub");
+        int operationNumber = Integer.parseInt(reader.readLine());
+        System.out.println("Input first number: ");
+        double firstNumber = Double.parseDouble(reader.readLine());
+        System.out.println("Input second number: ");
+        double secondNumber = Double.parseDouble(reader.readLine());
+
+        switch (operationNumber) {
+            case 1:
+                System.out.println(sum(firstNumber, secondNumber));
+                break;
+            case 2:
+                System.out.println(subtraction(firstNumber, secondNumber));
+                break;
+            default:
+                System.out.println("Operation number is not supported.");
+                break;
+        }
+    }
+
+    private double sum(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    private double subtraction(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+}
