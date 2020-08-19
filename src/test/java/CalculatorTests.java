710c00d4738fd68e94eefae4a88f252a4e3faff0
lerik14 <lerik140597@gmail.com>
2020-08-19 16:34:54 -0400

    Review fixes

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -1,89 +1,98 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTests {
+    private final Calculator calc = new Calculator();
+
     @Test
     public void sumOfTwoPositiveIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(3,5);
-        Assert.assertEquals("Should return the sum of two positive numbers", 8.0, sumResult, 0.0001);
+        double sumResult = calc.sum(3, 5);
+        assertEquals("Should return the sum of two positive numbers without fractional part", 8.0, sumResult, 0.0001);
     }
+
     @Test
     public void sumOfTwoNegativeIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(-3,-5);
-        Assert.assertEquals("Should return the sum of two negative numbers", -8.0, sumResult, 0.0001);
+        double sumResult = calc.sum(-3, -5);
+        assertEquals("Should return the sum of two negative numbers without fractional part", -8.0, sumResult, 0.0001);
     }
+
     @Test
     public void sumOfOneNegativeOnePositiveIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(3,-5);
-        Assert.assertEquals("Should return the sum of one positive and one negative numbers", -2.0, sumResult, 0.0001);
+        double sumResult = calc.sum(3, -5);
+        assertEquals("Should return the sum of one positive and one negative numbers without fractional part", -2.0, sumResult, 0.0001);
     }
+
     @Test
     public void sumOfTwoPositiveDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(3.6,5.35);
-        Assert.assertEquals("Should return the sum of two positive numbers", 8.95, sumResult, 0.0001);
+        double sumResult = calc.sum(3.6, 5.35);
+        assertEquals("Should return the sum of two positive numbers with fractional part", 8.95, sumResult, 0.0001);
     }
+
     @Test
     public void sumOfTwoNegativeDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(-3.5,-5.9);
-        Assert.assertEquals("Should return the sum of two negative numbers", -9.4, sumResult, 0.0001);
+        double sumResult = calc.sum(-3.5, -5.9);
+        assertEquals("Should return the sum of two negative numbers with fractional part", -9.4, sumResult, 0.0001);
     }
+
     @Test
     public void sumOfOneNegativeOnePositiveDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(3.57,-5.33);
-        Assert.assertEquals("Should return the sum of one positive and one negative numbers", -1.76, sumResult, 0.0001);
+        double sumResult = calc.sum(3.57, -5.33);
+        assertEquals("Should return the sum of one positive and one negative numbers with fractional part", -1.76, sumResult, 0.0001);
     }
-    @Test
-    public void sumOfTwoBoundaryValuesTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.sum(Double.MAX_VALUE,Double.MAX_VALUE);
-        Assert.assertEquals("Should return the sum of two boundary values", Double.POSITIVE_INFINITY, sumResult, 0.0001);
-    }
+
     @Test
     public void subtractionOfTwoPositiveIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(5,3);
-        Assert.assertEquals("Should return the subtraction of two positive numbers", 2.0, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(5, 3);
+        assertEquals("Should return the subtraction of two positive numbers without fractional part", 2.0, subtractionResult, 0.0001);
     }
+
     @Test
     public void subtractionOfTwoNegativeIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(-5,-3);
-        Assert.assertEquals("Should return the subtraction of two negative numbers", -2.0, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(-5, -3);
+        assertEquals("Should return the subtraction of two negative numbers without fractional part", -2.0, subtractionResult, 0.0001);
     }
+
     @Test
     public void subtractionOfOneNegativeOnePositiveIntNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(-5,3);
-        Assert.assertEquals("Should return the subtraction of One Negative and One Positive numbers", -8.0, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(-5, 3);
+        assertEquals("Should return the subtraction of One Negative and One Positive numbers without fractional part", -8.0, subtractionResult, 0.0001);
     }
+
     @Test
     public void subtractionOfTwoPositiveDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(5.8,3.2);
-        Assert.assertEquals("Should return the subtraction of two positive numbers", 2.6, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(5.8, 3.2);
+        assertEquals("Should return the subtraction of two positive numbers with fractional part", 2.6, subtractionResult, 0.0001);
     }
+
     @Test
     public void subtractionOfTwoNegativeDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(-5.6,-3.4);
-        Assert.assertEquals("Should return the subtraction of two negative numbers", -2.2, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(-5.6, -3.4);
+        assertEquals("Should return the subtraction of two negative numbers with fractional part", -2.2, subtractionResult, 0.0001);
     }
+
     @Test
     public void subtractionOfOneNegativeOnePositiveDoubleNumbersTest() {
-        Calculator calc = new Calculator();
-        double subtractionResult = calc.subtraction(-5.54,3.23);
-        Assert.assertEquals("Should return the subtraction of One Negative and One Positive numbers", -8.77, subtractionResult, 0.0001);
+        double subtractionResult = calc.subtraction(-5.54, 3.23);
+        assertEquals("Should return the subtraction of One Negative and One Positive numbers with fractional part", -8.77, subtractionResult, 0.0001);
     }
+
     @Test
-    public void subtractionOfTwoNegativeBoundaryValuesTest() {
-        Calculator calc = new Calculator();
-        double sumResult = calc.subtraction(Double.MIN_VALUE,Double.MIN_VALUE);
-        Assert.assertEquals("Should return the subtraction of two boundary values", 0, sumResult, 0.0001);
+    public void sumOfTwoBoundaryValuesTest() {
+        double sumResult = calc.sum(Double.MAX_VALUE, Double.MAX_VALUE);
+        assertEquals("Should return positive infinity when max double values passed", Double.POSITIVE_INFINITY, sumResult, 0.0001);
+    }
+
+    @Test
+    public void sumOfDoubleValueAndZero() {
+        double sumResult = calc.sum(5.234567, 0);
+        assertEquals("The same number should be returned, which is added with zero", 5.234567, sumResult, 0.0001);
+    }
+
+    @Test
+    public void subtractionOfDoubleValueAndZero() {
+        double subtractionResult = calc.sum(5.234567, 0);
+        assertEquals("The same number must be returned from which zero is subtracted", 5.234567, subtractionResult, 0.0001);
     }
 }
