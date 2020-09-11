a3b41a782d26776bcac9b81a7703008dee611701
lerik14 <lerik140597@gmail.com>
2020-09-11 13:33:57 -0400

    [CLC-10] Create JUnit runner class which will run all unit tests
    Added TestSuite class that runs all unit tests

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SimpleOperationsTests.class, TrigonometricOperationsTests.class})
+public class TestSuite {
+}
